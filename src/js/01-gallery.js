241d5ee0734fca02bc4c13377e81213de7009d91
Petrescu Alexandru <petrescu_robert94@yahoo.com>
2023-11-04 11:02:15 +0200

    Ex1 update and package.json

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Descris în documentație
+import SimpleLightbox from "simplelightbox";
+// Import suplimentar de stil
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryItemsEl = galleryItems
+    .map(({ preview, description, original }) =>
+        `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </div>`)
+    .join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryItemsEl);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250, captionPosition: "bottom", });
